374a9f510278a27c21b9526541de08e94006fe9e
saitotm <tomokazu.ml.tech@gmail.com>
2022-05-01 11:21:35 +0900

    implement merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
+#include <new>
 #include <sys/resource.h>
-
-void merge_sort(int *array, int size) {
-    // TODO: implement merge sort
-}
+#include <cassert>
 
 int cmp(int a, int b) {
     if (a < b) {
@@ -15,8 +13,8 @@ int cmp(int a, int b) {
     return 0;
 }
 
-bool check(int *array, int size) {
-    for (int i = 0; i < size - 1; ++i) {
+bool check(int *array, std::size_t size) {
+    for (std::size_t i = 0; i < size - 1; ++i) {
         if (cmp(array[i], array[i + 1]) == 1) {
             return false;
         }
@@ -33,17 +31,86 @@ void print_array(int *array, int size) {
     printf("]");
 }
 
-int main(int argc, char *argv[]) {
-    int array[5] = {3, 4, 2, 1, 9};
-    merge_sort(array, 5);
+void merge(int *out, const int *a, const int *b, std::size_t size_a, std::size_t size_b) {
+    std::size_t size_out = size_a + size_b;
+    std::size_t idx_a = 0;
+    std::size_t idx_b = 0;
 
-    bool is_ok = check(array, 5);
+    for (std::size_t i = 0; i < size_out; ++i) {
+
+        if (idx_a >= size_a) {
+            out[i] = b[idx_b];
+            idx_b++;
+            continue;
+        }
+
+        if (idx_b >= size_b) {
+            out[i] = a[idx_a];
+            idx_a++;
+            continue;
+        }
+
+        int c = cmp(a[idx_a], b[idx_b]);
+        if (c == -1) {
+            out[i] = a[idx_a];
+            idx_a++;
+        } else if (c == 1) {
+            out[i] = b[idx_b];
+            idx_b++;
+        } else {
+            out[i] = a[idx_a];
+            idx_a++;
+        }
+    }
+}
+
+void merge_sort(int *out, const int *array, std::size_t size) {
+    assert(size > 0);
+
+    if (size == 1) {
+        out[0] = array[0];
+        return;
+    }
+
+    std::size_t size_a = size / 2;
+    std::size_t size_b = size - size_a;
+    const int *a = array;
+    const int *b = array + size_a;
+
+    assert(size_a > 0);
+    assert(size_b > 0);
+
+    int *out_a = new int[size_a];
+    int *out_b = new int[size_b];
+
+    merge_sort(out_a, a, size_a);
+    assert(check(out_a, size_a));
+
+    merge_sort(out_b, b, size_b);
+    assert(check(out_b, size_b));
+
+    merge(out, out_a, out_b, size_a, size_b);
+
+    delete[] out_a;
+    delete[] out_b;
+}
+
+const int array_size = 9;
+
+int main(int argc, char *argv[]) {
+    int array[array_size] = {3, 4, 2, 1, 9, 10, 21, -3, 10};
+    int *out = new int[array_size];
+    merge_sort(out, array, array_size);
+
+    bool is_ok = check(out, array_size);
 
     if (is_ok) {
         printf("OK\n");
+        print_array(out, array_size);
+        printf("\n");
     } else {
         printf("Error\n");
-        print_array(array, 5);
+        print_array(out, array_size);
         printf("\n");
     }
 
